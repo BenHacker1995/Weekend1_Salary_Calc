2287077e6bbafce27de1a081d8594607cdd89bcb
Ben Hacker <bhacker1995@gmail.com>
2018-05-19 14:11:31 -0500

    Input button working but creating copies of previous inputs

--- scripts/client.js
+++ scripts/client.js
@@ -10,40 +10,71 @@ class Employee{
     } // end constructor
     } // end Employee
 
-    function onInputClick() {
-    // push new Employee inputs into employees array
-    console.log('in onInputClick');
+//     function onInputClick() {
+//     // push new Employee inputs into employees array
+//     console.log('in onInputClick');
 
-    employees.push( new Employee( $( '#firstName' ).val(), 
-                                    $( '#lastName' ).val(), 
-                                    $('#ID').val(),
-                                    $('#title').val(),
-                                    parseInt( $('#salary').val() ) ) );
-    console.log( employees );
-    let output = $('#outputTable');
-    // append new employee to table
-    for (employee of employees ){
-        output.not(':first').empty();
-        let outputVal = '<tr class="outputEl">';
-            outputVal += '<td>' + employee.firstName + '</td>';
-            outputVal += '<td>' + employee.lastName + '</td>';
-            outputVal += '<td>' + employee.ID + '</td>';
-            outputVal += '<td>' + employee.title + '</td>';
-            outputVal += '<td>' + employee.salary + '</td>';
-        outputVal += '</tr>';
-        // outputVal += '</tbody>';
-        output.append( outputVal );
-    }
-    $('#input').val(''); // clear inputs on inputButton click
-}
+//     employees.push( new Employee( $( '#firstName' ).val(), 
+//                                     $( '#lastName' ).val(), 
+//                                     $('#ID').val(),
+//                                     $('#title').val(),
+//                                     parseInt( $('#salary').val() ) ) );
+//     console.log( employees );
+//     let output = $('#outputTable');
+//     // append new employee to table
+//     for (employee of employees ){
+//         output.not(':first').empty();
+//         let outputVal = '<tr class="outputEl">';
+//             outputVal += '<td>' + employee.firstName + '</td>';
+//             outputVal += '<td>' + employee.lastName + '</td>';
+//             outputVal += '<td>' + employee.ID + '</td>';
+//             outputVal += '<td>' + employee.title + '</td>';
+//             outputVal += '<td>' + employee.salary + '</td>';
+//         outputVal += '</tr>';
+//         // outputVal += '</tbody>';
+//         output.append( outputVal );
+//     }
+//     $('#input').val(''); // clear inputs on inputButton click
+// }
 
 
 
 function readyNow() {
-    $('#input').val('');
-    $('.outputEl').remove();
+    // $('#input').val('');
+    // $('.outputEl').remove();
     
-    $('#submitButton').on( 'click', onInputClick())
+    // $('#submitButton').on( 'click', onInputClick())
 }
 
-$( document ).ready( readyNow() );
+$( document ).ready( function(){
+    // readyNow();
+    
+    $('#input').val('');
+    $('.outputEl').remove();
+    $('#submitButton').on( 'click', function() {
+            // push new Employee inputs into employees array
+            console.log('in onInputClick');
+        
+            employees.push( new Employee( $( '#firstName' ).val(), 
+                                            $( '#lastName' ).val(), 
+                                            $('#ID').val(),
+                                            $('#title').val(),
+                                            parseInt( $('#salary').val() ) ) );
+            console.log( employees );
+            let output = $('#outputTable');
+            // append new employee to table
+            for (employee of employees ){
+                output.not(':first').empty();
+                let outputVal = '<tr class="outputEl">';
+                    outputVal += '<td>' + employee.firstName + '</td>';
+                    outputVal += '<td>' + employee.lastName + '</td>';
+                    outputVal += '<td>' + employee.ID + '</td>';
+                    outputVal += '<td>' + employee.title + '</td>';
+                    outputVal += '<td>' + employee.salary + '</td>';
+                outputVal += '</tr>';
+                // outputVal += '</tbody>';
+                output.append( outputVal );
+            }
+            $('#input').val(''); // clear inputs on inputButton click
+    });
+});

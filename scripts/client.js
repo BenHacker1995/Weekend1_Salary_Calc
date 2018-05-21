721384fcbe7842ec9d6d237a890f9beda7842e76
Ben Hacker <bhacker1995@gmail.com>
2018-05-21 08:58:14 -0500

    Not able to remove employees from array as of yet, everything else works

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,5 @@
 let employees = []; // empty employees array
+let employeeCount = 0;
 let totalMonthly = 0;
 
 class Employee{
@@ -14,28 +15,46 @@ class Employee{
 $( document ).ready( function(){
     $( '.input' ).val( '' );
     $( '.outputEl' ).remove();
-    let totalOutput = $( '#outputAmount' );
-    document.getElementById( 'outputAmount' ).innerHTML = '<h3>Total Monthly: 0</h3>' ;
+    document.getElementById( 'outputAmount' ).innerHTML = '<h3>Total Monthly: ' + totalMonthly + '</h3>' ;
     $('#submitButton').on( 'click', function() {
             // push new Employee inputs into employees array
+        if ( $('#firstName').val() === '' ||
+             $('#lastName').val() === '' ||
+             $('#ID').val() === '' ||
+             $('#title').val() === '' ||
+             $('#salary').val() === '' ) {
+            alert('Invalid Input');
+            $('.input').val('');
+        }
+        else {
+            if ( parseInt( $('#salary').val() ) === 0 ){
+                $('#salary').val( 0 );
+            }
             employees.push( new Employee( $( '#firstName' ).val(), 
-                                            $( '#lastName' ).val(), 
-                                            $('#ID').val(),
-                                            $('#title').val(),
-                                            parseInt( $('#salary').val() ) ) );
-            console.log( employees );
+                                        $( '#lastName' ).val(), 
+                                        $('#ID').val(),
+                                        $('#title').val(),
+                                        parseInt( $('#salary').val() ) ) );
             runOutput();
             totalMonthly += parseInt( $('#salary').val() );
             document.getElementById( 'outputAmount' ).innerHTML = '<h3>Total Monthly: ' + totalMonthly+ '</h3>';
             if ( totalMonthly > 20000 ) {
                 document.getElementById( 'outputAmount' ).style.color = "red";
-            } // end if
+            } // end if            
             $('.input').val(''); // clear inputs on inputButton click
+        }
     });
 
         $('table').on('click', 'input[type="button"]', function(){
-            totalMonthly -= ($(this).closest('#salary')).val();
-            document.getElementById( 'outputAmount' ).innerHTML = '<h3>Total Monthly: ' + totalMonthly+ '</h3>';
+            // let closestSal = jQuery.data($(this).closest($('.Salary')));
+            let closestSal = $(this).closest('tr').find('.Salary').text();
+            let indexOut = $(this).closest('tr').index('tr');
+            
+            indexOut --;
+
+            employees.splice(indexOut, 1);
+            totalMonthly -= parseInt(closestSal);      
+            document.getElementById( 'outputAmount' ).innerHTML = '<h3>Total Monthly: ' + totalMonthly + '</h3>';
             $(this).closest('tr').empty();
 
     });
@@ -44,24 +63,15 @@ $( document ).ready( function(){
 
 function runOutput() {
     // add table row
-    let outputVal = `<tr id="${$('#firstName').val()}"class="outputEl">`
-        outputVal += '<td class="outputEl">' + $('#firstName').val() + '</td>';
-        outputVal += '<td class="outputEl">' + $('#lastName').val() + '</td>';
-        outputVal += '<td class="outputEl">' + $('#ID').val() + '</td>';
-        outputVal += '<td class="outputEl">' + $('#title').val() + '</td>';
-        outputVal += '<td class="outputEl">' + parseInt( $('#salary').val() ) + '</td>';
-        outputVal += '<td class="outputEl"><input type="button" value="Delete" class="btn-default"></td>';
+    let outputVal = `<tr id="${employeeCount}"class="outputEl">`
+        outputVal += `<td id="First${employeeCount}" class="First">` + $('#firstName').val() + '</td>';
+        outputVal += `<td id="Last${employeeCount}" class="Last">` + $('#lastName').val() + '</td>';
+        outputVal += `<td id="ID${employeeCount}" class="ID">` + $('#ID').val() + '</td>';
+        outputVal += `<td id="Title${employeeCount}" class="Title">` + $('#title').val() + '</td>';
+        outputVal += `<td id="Salary${employeeCount}" class="Salary">` + parseInt( $('#salary').val() ) + '</td>';
+        outputVal += `<td id="Button${employeeCount}" class="Button"><input type="button" value="Delete" class="btn-default"></td>`;
         outputVal += '</tr>';
+    employeeCount ++;
     $('tr:empty').remove();
     $('#inputOut').append( outputVal );
-    // append total monthly expenses
-
 } // end runOutput
-
-function deleteRow() {
-    // console.log('in deleteRow');
-    
-    
-}
-// function on click
-// removes current row entirely
